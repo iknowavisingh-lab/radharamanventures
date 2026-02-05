063f369b6e3b92cab8673d024f0f78209c32bd4f
iknowavisingh-lab <iknowavisingh@gmail.com>
2026-02-06 00:48:36 +0530

    Update and rename script.js to careers.html

--- script.js
+++
@@ -1,6 +0,0 @@
-document.getElementById("contactForm").addEventListener("submit", function(e) {
-    e.preventDefault();
-
-    alert("Thank you for contacting Radha Raman Ventures. We will get back to you soon.");
-    this.reset();
-});
