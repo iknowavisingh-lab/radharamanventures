f69e4648996654dce16204059dc86b7ba27880ee
iknowavisingh-lab <iknowavisingh@gmail.com>
2026-02-06 00:25:48 +0530

    Add form submission handler with alert

---
+++ script.js
@@ -0,0 +1,6 @@
+document.getElementById("contactForm").addEventListener("submit", function(e) {
+    e.preventDefault();
+
+    alert("Thank you for contacting Radha Raman Ventures. We will get back to you soon.");
+    this.reset();
+});
